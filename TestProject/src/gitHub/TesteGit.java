6216b4b1359461a3041cf679d122772fcaa89ad4
mutt76 <kabura100@gmail.com>
2023-08-24 17:29:15 +0100

    teste1

--- TestProject/src/gitHub/TesteGit.java
+++ TestProject/src/gitHub/TesteGit.java
@@ -3,7 +3,7 @@ package gitHub;
 public class TesteGit {
 
 	public static void main(String[] args) {
-		System.out.println("Hello World !");
+		System.out.println("Hello World !!");
 		//teste
 
 	}
